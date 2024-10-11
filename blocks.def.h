8a6e8aab8969352f9ef7e2dbb618e892b62284af
DannyCos <DannyCos@users.noreply.github.com>
2024-10-11 16:21:44 +0100

    Added blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,19 @@
+// #define PATH(name)			"/home/yigit/.local/bin/status-bar/"name
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	//{"", "/home/danny/.config/dwmblocks/scripts/network.sh",	2,		0},
+	//{"", "/home/danny/.config/dwmblocks/scripts/spotify.sh",	1,		6},
+	{"", "/home/danny/.config/dwmblocks/scripts/memory.sh",		10,		0},
+	//{"", "/home/danny/.config/dwmblocks/scripts/bitcoin.py",	600,		5},
+	{"", "/home/danny/.config/dwmblocks/scripts/volume.sh",		0,		4},
+	//{"", "/home/danny/.config/dwmblocks/scripts/weather.py",	3600,		3},
+	//{"", "/home/danny/.config/dwmblocks/scripts/packages.sh",	7200,		0},
+	//{"", "/home/danny/.config/dwmblocks/scripts/wifi.sh",		5,		2},
+	{"", "/home/danny/.config/dwmblocks/scripts/time.sh",		1,		1},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
-//sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " ";
 static unsigned int delimLen = 5;
